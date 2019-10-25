d663795438154695baa413954feb9e8c3b01666b
fjgarate <fgarate.dev@gmail.com>
2019-10-25 11:59:19 +0200

    url servidor openvidu3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  openvidu_url: 'https://video.emerge-mhealth.com:4443/',
+  openvidu_url: 'https://video.emerge-mhealth.com:4443',
   openvidu_secret: 'gbttel',
   api_url: 'https://login-videocall.herokuapp.com'
 };
