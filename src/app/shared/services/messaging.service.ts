c056a70ade2dc9d4222ccc29ac3d12664b919394
fjgarate <fgarate.dev@gmail.com>
2019-11-20 10:55:37 +0100

    push notification 5

--- src/app/shared/services/messaging.service.ts
+++ src/app/shared/services/messaging.service.ts
@@ -59,7 +59,7 @@ export class MessagingService {
     * hook method when new notification received in foreground
     */
   receiveMessage() {
-    this.angularFireMessaging.messaging.subscribe(
+    this.angularFireMessaging.messages.subscribe(
       (payload) => {
         console.log("new message received. ", payload);
         this.currentMessage.next(payload);
